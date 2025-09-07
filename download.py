4a622d18a923741c203b196c7545811654a9ec0b
Dr. Sanghita <sanghitabasu50@gmail.com>
2025-09-08 02:52:36 +0530

    Update download.py

--- download.py
+++ download.py
@@ -16,21 +16,15 @@ def create_output_folder(variable):
     path = os.path.join(BASE_DIR, variable)
     os.makedirs(path, exist_ok=True)
     return path
-
+    
 def generate_url(config, current_date, init_hour, forecast_hour, variable):
-    base_url = "https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl"
-    var_flags = {
-        "U1000": "var_UGRD=on&lev_1000_mb=on",
-        "V1000": "var_VGRD=on&lev_1000_mb=on",
-        "PREC": "var_PRATE=on&lev_surface=on"
-    }
-    return (
-        f"{base_url}?dir=%2Fgfs.{current_date.strftime('%Y%m%d')}%2F{init_hour:02d}%2Fatmos&"
-        f"file=gfs.t{init_hour:02d}z.pgrb2.0p25.f{forecast_hour:03d}&{var_flags[variable]}&"
-        f"subregion=&toplat=47&leftlon=55&rightlon=105&bottomlat=0"
-    )
-
-def download_grib_files(variable):
+    var_flag = config["VAR_FLAGS"][variable]
+    return (f"{config['BASE_URL']}?dir=%2Fgfs.{current_date.strftime('%Y%m%d')}%2F{init_hour:02d}%2Fatmos&"
+            f"file=gfs.t{init_hour:02d}z.pgrb2.0p25.f{forecast_hour:03d}&{var_flag}&"
+            f"subregion=&toplat={config['SUBREGION']['toplat']}&leftlon={config['SUBREGION']['leftlon']}&"
+            f"rightlon={config['SUBREGION']['rightlon']}&bottomlat={config['SUBREGION']['bottomlat']}")
+    
+def download_grib_files(config,variable):
     output_dir = create_output_folder(variable)
     current_date = datetime.now(timezone.utc)
     #current_date = datetime.now(timezone.utc) - timedelta(days=1)
@@ -50,5 +44,6 @@ def download_grib_files(variable):
                 else:
                     print(f"Failed to download ({response.status_code}): {url}")
     return output_dir
+
 
 

721bbfe0ef4ef3d174183c26cf9f9c76d56cb4d5
Dr. Sanghita <sanghitabasu50@gmail.com>
2025-09-08 17:53:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 import os
+from datetime import datetime, timezone
 from config import parse_args, build_config
 from download import download_grib_files
 from preprocessing import preprocess_variable, aggregate_and_save
@@ -13,30 +14,39 @@ def main():
     # Load config from arguments
     args = parse_args()
     config = build_config(args)
+    os.makedirs(cfg["PICKLE_DIR"], exist_ok=True)
+    os.makedirs(cfg["PLOT_DIR"], exist_ok=True)
+
+    current_date = datetime.now(timezone.utc)
 
     # Step 1: Download & Process GFS Data
     for variable in config["VARIABLES"]:
         print(f"[INFO] Processing Variable: {variable}")
-        output_dir = download_grib_files(config, variable)
+        output_dir = download_grib_files(config, variable, current_date)
         daily_data = preprocess_variable(variable, output_dir, config)
-        aggregate_and_save(daily_data, variable, config)
+        if variable=="PREC":
+            daily_data = preprocess_precipitation(config, variable, out_dir)
+        else:
+            daily_data = preprocess_wind(config, variable, out_dir)
+        aggregate_and_save(config, daily_data, variable)
 
     # Step 2: PCA & Pickle
     for lday in config["LEAD_DAYS"]:
-        preprocess_and_save_pca(lday, config)
+        preprocess_and_save_pca(config, lday)
 
     # Step 3: Forecast + Plot
     pred_series_list = []
     for lday in config["LEAD_DAYS"]:
-        for result in forecast_future_rain(lday, config):
+        for result in forecast_future_rain(config, lday):
             pred_series_list.append(result)
 
     if pred_series_list:  # only plot if we actually have forecasts
-        plot_last_3_days_bar(pred_series_list, config)
+        plot_last_3_days_bar(config, pred_series_list)
     else:
         print("[WARNING] No forecasts available to plot.")
 
 
 if __name__ == "__main__":
     main()
+
 
